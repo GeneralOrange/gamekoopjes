9cae02d81c3961f8df004a08038e6a418182e5fd
General Orange <erik.17021995@gmail.com>
2019-08-24 01:39:12 +0200

    Optimized even furter

--- functions.php
+++ functions.php
@@ -120,11 +120,11 @@ add_action( 'widgets_init', 'gamekoopjes_widgets_init' );
  * Enqueue scripts and styles.
  */
 function gamekoopjes_scripts() {
-	wp_enqueue_style( 'gamekoopjes-style', get_stylesheet_uri() );
+	wp_enqueue_style('google-font', 'https://fonts.googleapis.com/css?family=Lato', true);
 
-	wp_enqueue_script( 'gamekoopjes-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_style( 'gamekoopjes-style', get_template_directory_uri() . '/assets/dist/css/style.css');
 
-	wp_enqueue_script( 'gamekoopjes-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'gamekoopjes-js', get_template_directory_uri() . '/assets/dist/js/app.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
