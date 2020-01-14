f4972a6ccc6a50b7925eb9be16ea26c6bf0134ac
General Orange <erik.17021995@gmail.com>
2020-01-14 22:07:40 +0100

    Just a few tweaks

--- comments.php
+++ comments.php
@@ -21,55 +21,59 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php
+				// You can start editing here -- including this comment!
+				if ( have_comments() ) :
+					?>
+					<h2 class="comments-title">
+						<?php
+						$gamekoopjes_comment_count = get_comments_number();
+						if ( '1' === $gamekoopjes_comment_count ) {
+							printf(
+								/* translators: 1: title. */
+								esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'gamekoopjes' ),
+								'<span>' . get_the_title() . '</span>'
+							);
+						} else {
+							printf( // WPCS: XSS OK.
+								/* translators: 1: comment count number, 2: title. */
+								esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $gamekoopjes_comment_count, 'comments title', 'gamekoopjes' ) ),
+								number_format_i18n( $gamekoopjes_comment_count ),
+								'<span>' . get_the_title() . '</span>'
+							);
+						}
+						?>
+					</h2><!-- .comments-title -->
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$gamekoopjes_comment_count = get_comments_number();
-			if ( '1' === $gamekoopjes_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'gamekoopjes' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $gamekoopjes_comment_count, 'comments title', 'gamekoopjes' ) ),
-					number_format_i18n( $gamekoopjes_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+					<?php the_comments_navigation(); ?>
 
-		<?php the_comments_navigation(); ?>
+					<ol class="comment-list">
+						<?php
+						wp_list_comments( array(
+							'style'      => 'ol',
+							'short_ping' => true,
+						) );
+						?>
+					</ol><!-- .comment-list -->
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
-			?>
-		</ol><!-- .comment-list -->
+					<?php
+					the_comments_navigation();
 
-		<?php
-		the_comments_navigation();
+					// If comments are closed and there are comments, let's leave a little note, shall we?
+					if ( ! comments_open() ) :
+						?>
+						<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'gamekoopjes' ); ?></p>
+						<?php
+					endif;
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'gamekoopjes' ); ?></p>
-			<?php
-		endif;
+				endif; // Check for have_comments().
 
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
-
+				comment_form();
+				?>
+			</div>
+		</div>
+	</div>
 </div><!-- #comments -->
